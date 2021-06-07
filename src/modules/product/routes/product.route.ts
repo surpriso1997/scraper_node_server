998e6159cb04277f3d28519854fcba22510ec3be
manish <maneesh10chaulagain@gmail.com>
2021-06-07 19:18:34 +0545

    remove imports

--- src/modules/product/routes/product.route.ts
+++ src/modules/product/routes/product.route.ts
@@ -1,7 +1,6 @@
 import Route from '@/interfaces/routes.interface';
 import { Router } from 'express';
 import ProductController from '../controller/product.controller';
-import { ProductService } from '../service/product.service';
 
 class ProductRoute implements Route {
   public path = '/product';
