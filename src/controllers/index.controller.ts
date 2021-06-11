010adeeb92df413952f09c74196a149f5fd1c4aa
manish <maneesh10chaulagain@gmail.com>
2021-06-11 17:07:50 +0545

    change homeapge response

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      return res.status(200).json({ this: 'is home route' });
     } catch (error) {
       next(error);
     }
